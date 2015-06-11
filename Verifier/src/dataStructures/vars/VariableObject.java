74c6ecc6a0a7f386fcf66b9aa8445fade2c9b297
yonilip <johnnyhalen@gmail.com>
2015-06-11 21:44:02 +0300

    ahhh eclipse!!!!

--- Verifier/src/dataStructures/vars/VariableObject.java
+++ Verifier/src/dataStructures/vars/VariableObject.java
@@ -2,8 +2,10 @@
  * Created by rooty on 6/11/15.
  */
 package dataStructures.vars;
+
 public class VariableObject {
 }
 
 // this is git test1
 // Fix for issue
+// Hi ziv, I think this now works as a java file in eclipse, trying the shit im learning here
