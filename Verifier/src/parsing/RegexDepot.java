afbe5b386bbf32a0c6e5d3606b725813b95357ca
ziv <zivbenster@gmail.com>
2015-06-19 00:16:55 +0300

    no push list

--- Verifier/src/parsing/RegexDepot.java
+++ Verifier/src/parsing/RegexDepot.java
@@ -7,18 +7,16 @@ import java.util.regex.Pattern;
  */
 public class RegexDepot {
 
-	public final static String VAR_NAME = " ?[a-zA-Z]+[\\w]*|[_][\\w]+"; // TODO why is there a space here in the beginning?
-	public final static String VAR_VALUE = "(" + VAR_NAME + ") ?= ?(.+)";
+	public static final String VARIABLE_NAME=" ?[a-zA-Z]+[\\w]*|[_][\\w]+";
+	public static final String VARIABLE_ASSIGNEMENT = "(" + VARIABLE_NAME + ") ?= ?(.+)";
 
-	public final static Pattern SCOPE_PATTERN = Pattern.compile("void|if|while");
-	public final static Pattern VAR_PATTERN = Pattern.compile(VAR_NAME);
-	public final static Pattern METHOD_DECLERATION =
-			Pattern.compile("(final )?(int|double|String|boolean|char) (" + VAR_NAME + ")"); // TODO did you mean to put scope pattern in here?
-	public final static Pattern VAR_DECLERATION =
+	public static final Pattern VARIABLE_PATTERN = Pattern.compile(VARIABLE_NAME);
+	public static final Pattern VARIABLE_DECLARATION_PATTERN =
 			Pattern.compile("(final )?(int|double|String|boolean|char) ([^;]+);");
-	public final static Pattern VAR_ASSIGN = Pattern.compile(VAR_VALUE + " ?;");
-	public final static Pattern METHOD_CALL_PATTERN = Pattern.compile("([a-zA-Z]+[\\w]*) ?\\((.*)\\) ?;");
-	public final static Pattern RETURN_PATTERN = Pattern.compile("return ?;");
-	public final static Pattern CONDITION_PATTERN = Pattern.compile("(if|while) ?\\((.+)\\) ?\\{");
-	public final static Pattern METHOD_PATTERN = Pattern.compile("void ([a-zA-Z]+[\\w]*) ?\\((.*)\\) ?\\{");
+	public static final Pattern VARIABLE_ASSIGNEMENT_PATTERN = Pattern.compile(VARIABLE_ASSIGNEMENT + " ?;");
+	public static final Pattern METHOD_CALL_PATTERN = Pattern.compile("([a-zA-Z]+[\\w]*) ?\\((.*)\\) ?;");
+	public static final Pattern RETURN_PATTERN = Pattern.compile("return ?;");
+	public static final Pattern CONDITION_PATTERN = Pattern.compile("(if|while) ?\\((.+)\\) ?\\{");
+	public static final Pattern METHOD_PATTERN= Pattern.compile("void ([a-zA-Z]+[\\w]*) ?\\((.*)\\) ?\\{");
+
 }
